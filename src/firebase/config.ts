12588773ef39a7670e983d0915fd86624f1c7675
Cassi Apps <appscassi@gmail.com>
2026-03-15 21:19:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9374703561-162ae",
+  "appId": "1:1046641945724:web:0acd0a10c6d8d7b6b26df9",
+  "apiKey": "AIzaSyDflx-EdD7m9e5X54C61nmlnso0coMJdG0",
+  "authDomain": "studio-9374703561-162ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1046641945724"
+};
